77f1ff617e70e008a2c994ea8de82ae60b84fae0
shary <shary@wirelessprv-10-192-142-77.near.illinois.edu>
2017-12-09 16:08:03 -0600

    init

---
+++ src/main/java/com/emusicstore/controller/HomeController.java
@@ -0,0 +1,46 @@
+package com.emusicstore.controller;
+
+import com.emusicstore.dao.ProductDao;
+import com.emusicstore.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.io.IOException;
+import java.util.List;
+
+
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private ProductDao productDao;
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+
+
+    @RequestMapping("/productList")
+    public String getProducts(Model model) {
+        List<Product> products = productDao.getAllProducts();
+        model.addAttribute("products", products);
+
+        return "productList";
+    }
+
+//    @RequestMapping("/productList/viewProduct/{productId}")
+//    public String viewProduct(@PathVariable String productId, Model model) throws IOException{
+//
+//        Product product = productDao.getProductById(productId);
+//        model.addAttribute(product);
+//
+//        return "viewProduct";
+//    }
+}
